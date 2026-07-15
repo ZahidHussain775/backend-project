9c70e7a698f3e7f8f55adf16694f4814c4bd8700
ZahidHussain775 <mirzahidhussain322@gmail.com>
2026-07-15 12:33:38 +0500

    Add Multer for local file upload functionality

--- routes/index.route.js
+++ routes/index.route.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const upload = require('../config/multer.config');
 
 const router = express.Router();
 
 router.get('/home',(req,res) => {
     res.render("Home")
-})
+});
+
+router.post('/upload', upload.single('file'), (req, res) => {
+    if (!req.file) {
+        return res.status(400).send('No file uploaded.');
+    }
+    console.log('File uploaded:', req.file); 
+     res.send('File uploaded successfully.');
+});
+
+
     
 
 
