997e0e9acd37adc01b9a2cf9a0c65a868d8d5ece
ZahidHussain775 <mirzahidhussain322@gmail.com>
2026-07-19 20:48:22 +0500

    feat: add Notes REST API for Thunder Client testing

--- app.js
+++ app.js
@@ -21,6 +21,50 @@ app.use("/user", userRoutes);
 app.use("/", indexRoutes);
 
 
+// Notes API  testing through thunder client 
+const notes = []
+
+app.post('/notes' , (req,res) => {
+
+    notes.push(req.body);
+
+   res.status(201).json({message: "Note added successfully", notes});
+})
+
+app.get('/notes', (req,res) => {
+    res.status(200).json({
+        message: "Notes retrieved successfully",
+        notes: notes
+      });
+});
+
+app.delete('/notes/:index', (req,res) => {
+  
+  const index = req.params.index;
+  delete notes[index];
+
+  res.status(200).json({
+    message: "Note deleted successfully",
+      });
+
+});
+
+
+app.patch('/notes/:index', (req,res) => {
+  
+  const index = req.params.index;
+  const description = req.body.description;
+
+  notes[index].description = description;
+
+  res.status(200).json({
+    message: "Note updated successfully",
+  
+  });
+});
+
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 })
+
